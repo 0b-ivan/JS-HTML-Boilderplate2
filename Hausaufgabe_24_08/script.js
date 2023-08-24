b87cb08192287f3c0fa12e9f378d53dc52822e74
Lev2607 <levent.o@web.de>
2023-08-24 13:59:23 +0200

    index.html und script.js erstellen

---
+++ Hausaufgabe_24_08/script.js
@@ -0,0 +1,8 @@
+// script.js
+
+// Das HTML-Fragment erstellen
+var htmlFragment = '<h1>Es hat funktioniert!</h1>' +
+                   '<p>Ich bin so froh!</p>';
+
+// Das HTML-Fragment in das Dokument schreiben
+document.write(htmlFragment);
