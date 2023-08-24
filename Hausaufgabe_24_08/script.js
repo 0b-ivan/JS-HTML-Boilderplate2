caf241818419e5de005eb28eea8109752c816d6d
dino505 <dino.milje@gmail.com>
2023-08-24 13:56:07 +0200

    Added Hausaufgabe_24_08 folder with index.html and script.js files

---
+++ Hausaufgabe_24_08/script.js
@@ -0,0 +1,2 @@
+document.write("<h1>It worked, goddaaaaayuuum!</h1>");
+document.write("<p>I'm so Happy and LuKas is gonna be happy too. AP baby blue, paper's pink, I'd probably hate me too. Look what you made me do.</p>");
