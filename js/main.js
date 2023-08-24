eedbe2667c81d70e9ce5d9fb508e0c00df94f02b
dyprodg <dennisdiepolder@outlook.com>
2023-08-24 11:47:10 +0200

    rechnung

--- js/main.js
+++ js/main.js
@@ -1,2 +1,5 @@
 document.write("<div><h1>Hallo Welt!<h1></div>");
 document.write("<span style='color: #FF0000'>Rot</span>");
+document.write("hier wird 3 + 5 gerechnet!");
+document.write("3 + 5 = ");
+document.write(3 + 5);
