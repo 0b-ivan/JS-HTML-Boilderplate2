588c36ca5de71238394f293c34ed7df5a89f8ba8
Lev2607 <levent.o@web.de>
2023-08-25 09:51:14 +0200

    Add Math example

--- js/main.js
+++ js/main.js
@@ -1,2 +1,8 @@
 document.write("<div><h1>Hallo Welt!<h1></div>");
+<<<<<<< HEAD
 document.write("<span style='color: #FF0000'>Rot</span>");
+=======
+document.write("<span style='color: #FF0000'>Rot</span>");
+document.write("3 + 5 = ")
+document.write(3 + 5)
+>>>>>>> dc2ef40 (Add Math example)
